696aaf7e3044d6b8802213e718ec91c740692632
Hassan709-prog <hassancs709@gmail.com>
2026-08-16 11:56:20 -0700

    feat: implement deterministic URL normalization

--- packages/scanner/src/email_scanner/__init__.py
+++ packages/scanner/src/email_scanner/__init__.py
@@ -5,10 +5,12 @@ from email_scanner.errors import (
     URLNormalizationErrorCode,
 )
 from email_scanner.models import HostType, NormalizedURL
+from email_scanner.normalization import normalize_url
 
 __all__ = [
     "HostType",
     "NormalizedURL",
     "URLNormalizationError",
     "URLNormalizationErrorCode",
+    "normalize_url",
 ]
